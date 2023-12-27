29e0dfe19b81e7c0aebd1b2ec105ecf9b2f5ba34
Unown-Sapphire <72756485+Unown-Sapphire@users.noreply.github.com>
2023-12-27 22:18:17 +0530

    Made it so it gets text from a test subreddit

--- subreddit.py
+++ subreddit.py
@@ -4,4 +4,13 @@ import praw
 with open("Reddit API/redditapiclient.json") as json_data:
     redditkeys = json.load(json_data)
 
-    
+reddit = praw.Reddit(
+    client_id = "-4WA2RrwmCqDytJam4HnzA",
+    client_secret = "oVVMVjxSiUr0iGF6NVYF2zQawk13Jw",
+    user_agent = "subfetcherv1"
+)
+
+subreddit = reddit.subreddit("test")
+
+for submission in subreddit.top(limit=2):
+    print(submission.selftext)
