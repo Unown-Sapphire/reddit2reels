0a0fcc9f3cbb5314cf31d0cc5fdf1e03ead06e72
Unown-Sapphire <72756485+Unown-Sapphire@users.noreply.github.com>
2023-12-27 19:58:18 +0530

    Changes the Text from the docs to own text

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from moviepy.editor import * # type: ignore
 clip = VideoFileClip("example.mp4") #example clip
 
 #Cropping Clips
-sub_clip = clip.subclip(0, 100) #crops the clip from 0 to 2 seconds
+sub_clip = clip.subclip(0, 100) #crops the clip from 0 to 100 seconds
 
 #Adjusting volume of sub_clip idrk why it doesnt work for VideoFileClip
 volume_clip = sub_clip.volumex(0.8)
@@ -11,4 +11,4 @@ volume_clip = sub_clip.volumex(0.8)
 txt_clip = TextClip("My Holidays 2013",fontsize=70,color='white')
 txt_clip = txt_clip.set_duration(10)
 
-video = CompositeVideoClip([clip, txt_clip]) #mixing the 
+video = CompositeVideoClip([clip, txt_clip]) #mixing the clip and video clip 
