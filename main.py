fc7c2d2183cc85b91e3daec1d6c89b692c72807c
UnoSapphire <72756485+Unown-Sapphire@users.noreply.github.com>
2023-12-27 19:38:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from moviepy.editor import * # type: ignore
+
+clip = VideoFileClip("example.mp4") #example clip
+
+#Cropping Clips
+sub_clip = clip.subclip(0, 100) #crops the clip from 0 to 2 seconds
+
+#Adjusting volume of sub_clip idrk why it doesnt work for VideoFileClip
+volume_clip = sub_clip.volumex(0.8)
+
+txt_clip = TextClip("My Holidays 2013",fontsize=70,color='white')
+txt_clip = txt_clip.set_duration(10)
+
+video = CompositeVideoClip([clip, txt_clip]) #mixing the 
