b6a3d682bc955210e4fe7685a0ba4ff9fda415e6
Unown-Sapphire <72756485+Unown-Sapphire@users.noreply.github.com>
2023-12-27 19:59:05 +0530

    Changing the doc text to normal text

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ sub_clip = clip.subclip(0, 100) #crops the clip from 0 to 100 seconds
 #Adjusting volume of sub_clip idrk why it doesnt work for VideoFileClip
 volume_clip = sub_clip.volumex(0.8)
 
-txt_clip = TextClip("My Holidays 2013",fontsize=70,color='white')
+txt_clip = TextClip("Example Text",fontsize=70,color='white')
 txt_clip = txt_clip.set_duration(10)
 
 video = CompositeVideoClip([clip, txt_clip]) #mixing the clip and video clip 
