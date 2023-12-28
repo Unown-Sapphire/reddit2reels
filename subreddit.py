007dc4951b077232335e50c79fbb9e971fa1bbb1
unown-sapphire <dmvarun06@gmail.com>
2023-12-28 16:19:41 +0530

    modified the subreddit.py code a little, will randomise the subreddits now

--- subreddit.py
+++ subreddit.py
@@ -14,13 +14,18 @@ reddit = praw.Reddit(
 
 post_list = []
 
+subreddit_list = ["AmITheAsshole", "tifu", ]
+
 subreddit = reddit.subreddit("AmITheAsshole")
 
-for submissions in subreddit.top(limit=20):
+for submissions in subreddit.hot(limit=20):
     post_list.append(submissions.selftext)
+
+print(submissions.title)
 
 random_post = random.choice(post_list)
 print(random_post)
 
 with open('randompost.txt', 'w', encoding='utf-8') as post:
+    post.write(submissions.title)
     post.write(random_post)
