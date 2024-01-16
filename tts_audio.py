6ddcc6da0aa79f595b2142536903e59161ccc6ac
unown-sapphire <dmvarun06@gmail.com>
2024-01-16 21:40:53 +0530

    whisperAI added

--- tts_audio.py
+++ tts_audio.py
@@ -1,8 +1,15 @@
-import gtts
+from openai import * 
+client  = OpenAI(
+    api_key="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"
+)
 
-with open ("randompost.txt") as randomposts:
-    posts = randomposts.read()
+headers = {
+    "Content-Type" : "text/plain",
+    "Authorization": "Bearer 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"
+}
 
-tts = gtts.gTTS(str(posts), lang="en-us", tld="us")
-
-tts.save('test.mp3')
+audio_file= open("test.mp3", "rb")
+transcript = client.audio.transcriptions.create(
+  model="whisper-1", 
+  file=audio_file
+)
