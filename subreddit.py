d8e4c6216a7a15529f2ef82f1d09a93e4797e770
Unown-Sapphire <72756485+Unown-Sapphire@users.noreply.github.com>
2024-04-08 18:34:23 +0530

    implemented gTTS as the new TTS and working on subreddit.py

--- subreddit.py
+++ subreddit.py
@@ -28,7 +28,13 @@ random_title = title_list[post_list.index(random_post)]
 print(random_title)
 print(random_post)
 
-with open('randompost.txt', 'w', encoding='utf-8') as post:
+with open('randompost.txt', 'r+', encoding='utf-8') as post:
     post.write(random_title)
     post.write('\n')
     post.write(random_post)
+    lines = post.readlines()
+    word = "AITA"
+    for line in lines:
+        if line.find("AITA") != -1:
+            print(word, 'string exists in file')
+            new_line = line.replace('AITA', "Am I the asshole")
--- tts_audio.py
+++ tts_audio.py
@@ -1,16 +1,8 @@
-from openai import * 
-client  = OpenAI(
-    api_key="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"
-)
+import gtts
 
-headers = {
-    "Content-Type" : "text/plain",
-    "Authorization": "Bearer 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"
-}
+with open ("randompost.txt", encoding='utf-8') as randomposts:
+    posts = randomposts.read()
 
-audio_file= open("test.mp3", "rb")
-transcript = client.audio.transcriptions.create(
-  model="whisper-1", 
-  file=audio_file
-)
-print(transcript)
+tts = gtts.gTTS(str(posts), lang="en-us", tld="us")
+
+tts.save('test.mp3')
