1a55ea798700eee74a8b76d8705c1aac77c1fb77
Unown-Sapphire <72756485+Unown-Sapphire@users.noreply.github.com>
2024-05-30 15:28:31 +0530

    Made changes to properties of subtitles

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from moviepy.editor import * # type: ignore
 from moviepy.video.tools.subtitles import SubtitlesClip
 
-import subreddit
-print("Wow1")
-import tts_audio
-print("Wow2")
+# import subreddit
+# print("Wow1")
+# import tts_audio
+# print("Wow2")
 
 def videoEditing():
     videoclip = VideoFileClip("videos/example.mp4")
@@ -14,11 +14,11 @@ def videoEditing():
     videoclip.audio = new_audioclip
 
     sub_clip = videoclip.subclip(0, 64) #crops the clip from 0 to 100 seconds
-    generator = lambda txt: TextClip(txt, font='Arial', fontsize=24, color='white')
+    generator = lambda txt: TextClip(txt, font='Arial', fontsize=80, color='white', method="caption", stroke_color="black", stroke_width=2, size=(1080, None))
     subs = SubtitlesClip('spedup.srt', generator)
     subtitles = SubtitlesClip(subs, generator)
-    sub_clip = CompositeVideoClip([videoclip, subtitles.set_pos(('center','bottom'))])
-    sub_clip.write_videofile("videos/export.mp4")
+    sub_clip = CompositeVideoClip([videoclip, subtitles.set_pos(('center','center'))])
+    sub_clip.write_videofile("videos/export.mp4", codec="libx264")
     print("Wow3")
 
 videoEditing()
